7321ee8a61c25250aa62ef85bd58d0e31eabf32c
Jack Birger <jack.birger@gmail.com>
2015-04-21 22:03:41 -0400

    wrangle data function for the circle packing display

--- code/js/detailVis.js
+++ code/js/detailVis.js
@@ -20,13 +20,74 @@ DetailVis.prototype.update = function(stop_id, category, radius){
 	}
 	$('#detailVis').html('<div class="location-name">' + station + '</div><div class="restaurants"></div>');
 
-	displayData = wrangleData(stop_id, category, radius);
+	displayData = this.wrangleData(stop_id, category, radius, station);
+	console.log("Display Data", displayData)
 }
 
-// DetailVis.prototype.wrangleData = function(stop_id, category, radius) {
-// 	for (i=0;i<this.data.length; i++) {
-// 		if (this.data[i]['stop_id'] == stop_id) {
-// 			// do stuff
-// 		} 
-// }
+DetailVis.prototype.wrangleData = function(stop_id, category, radius, station) {
+
+	
+	this.category_list = category_list
+	var that = this;
+
+	this.stopData =[]
+
+	//Store an array of the restaurants for the selected stop
+	for (i=0;i<this.data.length; i++) {
+		if (this.data[i]['stop_id'] == stop_id) {
+			this.stopData.push(this.data[i])
+		} 
+	}
+
+	console.log(this.stopData)	//array of restaurants for the given stop
+
+
+	//initialize second level
+	this.category_level = []
+	category_list.forEach(function(cat){
+		 that.temp = {
+			"name": cat,				//The name of the category
+			"children": []				//Array holding object with restaurant "name" and the "size" (rating)
+		}
+
+		that.category_level.push(that.temp)
+	})
+
+
+	//initialize first level
+	this.nested_data = {
+		"name": 0,						//The ID of the selected Train Stop
+		"children":this.category_level		//Array hoolding object of categories & children
+		
+	}
+
+	//Populate category level with the correct restaurants
+	this.stopData.forEach(function(restaurant){
+
+		restaurant.categories.forEach(function(cat){
+			that.category_level.forEach(function(nestCat){
+				if(cat == nestCat["name"]){
+					nestCat["children"].push({"name":restaurant.name, "size":restaurant.rating});	
+				}
+			})
+
+		})
+
+	})
+
+	this.nested_data["name"] = station
+
+	//console.log("nester array", this.nested_data)
+
+	return this.nested_data
+
+}
+
+
+
+
+
+
+
+
 
