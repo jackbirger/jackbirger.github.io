c02d1c929269d76c93ccda69293aed4c5de70dd5
Jack Birger <jack.birger@gmail.com>
2015-04-21 22:49:33 -0400

    Added circle packing function although not working properly

--- code/js/detailVis.js
+++ code/js/detailVis.js
@@ -22,8 +22,12 @@ DetailVis.prototype.update = function(stop_id, category, radius){
 
 	displayData = this.wrangleData(stop_id, category, radius, station);
 	console.log("Display Data", displayData)
+
+	this.displayCirclePacking(displayData, stop_id, category, radius, station);
 }
 
+
+//Wrangle the data for the circle packing function
 DetailVis.prototype.wrangleData = function(stop_id, category, radius, station) {
 
 	
@@ -84,10 +88,55 @@ DetailVis.prototype.wrangleData = function(stop_id, category, radius, station) {
 }
 
 
+//Create Circle Packing layout
+DetailVis.prototype.displayCirclePacking = function(displayData, stop_id, category, radius, station){
+
+/////////////////////////////////////////////////
+//Based on http://bl.ocks.org/mbostock/4063530
+////////////////////////////////////////////////
+var diameter = 1000,
+    format = d3.format(",d");
+
+var pack = d3.layout.pack()
+    .size([diameter - 4, diameter - 4])
+    .value(function(d) { return d.size; });
+
+var svg = d3.select("#circle-packing")
+  .append("g")
+    .attr("transform", "translate(2,2)");
+
+
+var node = svg.datum(displayData).selectAll(".node")
+  .data(pack.nodes)
+.enter().append("g")
+  .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+  .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+node.append("title")
+  .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
+
+node.append("circle")
+  .attr("r", function(d) { return d.r; });
+
+node.filter(function(d) { return !d.children; }).append("text")
+  .attr("dy", ".3em")
+  .style("text-anchor", "middle")
+  .text(function(d) { return d.name.substring(0, d.r / 3); });
+
+
+//d3.select(self.frameElement).style("height", diameter + "px");
 
 
 
 
+
+
+
+}
+
+
+
+//style="background-color: lightblue"
 
 
 
