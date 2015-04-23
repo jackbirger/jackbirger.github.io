2d142374a6acb1e59d197527a753f4a06fe5d622
Jack Birger <jack.birger@gmail.com>
2015-04-22 23:07:36 -0400

    Final changes to enable circle packing

--- code/js/detailVis.js
+++ code/js/detailVis.js
@@ -104,34 +104,35 @@ DetailVis.prototype.wrangleData = function(stop_id, category, radius, station) {
 //Create Circle Packing layout
 DetailVis.prototype.displayCirclePacking = function(displayData, stop_id, category, radius, station){
 
+this.displayData = displayData
 /////////////////////////////////////////////////
 //Based on http://bl.ocks.org/mbostock/4063530
 ////////////////////////////////////////////////
-var diameter = 1000,
+this.diameter = 650,
     format = d3.format(",d");
 
-var pack = d3.layout.pack()
-    .size([diameter - 4, diameter - 4])
+this.pack = d3.layout.pack()
+    .size([this.diameter - 4, this.diameter - 4])
     .value(function(d) { return d.size; });
 
-var svg = d3.select("#circle-packing")
+this.svg = d3.select("#packing")
   .append("g")
     .attr("transform", "translate(2,2)");
 
 
-var node = svg.datum(displayData).selectAll(".node")
-  .data(pack.nodes)
+this.node = this.svg.datum(this.displayData).selectAll(".node")
+  .data(this.pack.nodes)
 .enter().append("g")
   .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
   .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-node.append("title")
+this.node.append("title")
   .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
 
-node.append("circle")
+this.node.append("circle")
   .attr("r", function(d) { return d.r; });
 
-node.filter(function(d) { return !d.children; }).append("text")
+this.node.filter(function(d) { return !d.children; }).append("text")
   .attr("dy", ".3em")
   .style("text-anchor", "middle")
   .text(function(d) { return d.name.substring(0, d.r / 3); });
