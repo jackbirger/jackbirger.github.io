5f1f6d7f001a790afc022ebc70ba40c36f2cfdcf
Christian A. Schlubach <christian.schlubach@gmail.com>
2015-04-26 13:08:52 -0400

    Added files to contain vis js code

---
+++ code/js/scatterVis.js
@@ -0,0 +1,5 @@
+scatterVis = function(_parentElement, _data, _metaData){
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.metaData = _metaData;
+}
---
+++ code/js/zoomVis.js
@@ -0,0 +1,5 @@
+zoomVis = function(_parentElement, _data, _metaData){
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.metaData = _metaData;
+}
