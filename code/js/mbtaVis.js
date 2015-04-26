8e737b559acf6341085ab9134da477f43a3792c6
Christian A. Schlubach <christian.schlubach@gmail.com>
2015-04-26 19:20:16 -0400

    cleaned up mbtaVis

--- code/js/mbtaVis.js
+++ code/js/mbtaVis.js
@@ -4,45 +4,28 @@ mbtaVis = function(_parentElement, _data, _metaData){
     this.data = _data;
     this.metaData = _metaData;
 
-    this.initVis();
-}
+	var stop_list = [];  //Aggregate data list
+	this.radius_scale = d3.scale.linear().range([0,60]);
+	//var circles = d3.selectAll('circle')
+	var encode_category='all_cat'; // set this to all categories to start
 
-//Initialize circles using metadata
-mbtaVis.prototype.initVis = function() {
+	// draw the initial circles
+	metaData.forEach(function(stop){
 
-  var stop_list = [];  //Aggregate data list
-  var radius_scale = d3.scale.linear().range([0,60]);
-  var circles = d3.selectAll('circle')
-  var encode_category='all_cat'; // set this to all categories to start
-
-  dv = new DetailVis('#detailVis', allData, metaData);
-
-  metaData.forEach(function(stop){
-
-     d3.select("#circles").append('circle')
-        .attr("cx", stop['x'])
-        .attr("cy", stop['y'])
-        .attr("r", 10)
-        .attr("class", function(){
-           if(stop.line.length == 1){
-              return stop.line[0];
-           } else{
-              return stop.line[0] + ' ' + stop.line[1]
-           }
-        })
-        .attr("id", stop["station"])
-        .datum(stop);
-        
-
-  })
-
-  //Call function to calculate totals
-  aggregateTotals()
-  }
-
-    //Aggregate the values for count and average ratings
-   var aggregateTotals = function(){
-
+	 d3.select("#circles").append('circle')
+	    .attr("cx", stop['x'])
+	    .attr("cy", stop['y'])
+	    .attr("r", 10)
+	    .attr("class", function(){
+	       if(stop.line.length == 1){
+	          return stop.line[0];
+	       } else{
+	          return stop.line[0] + ' ' + stop.line[1]
+	       }
+	    })
+	    .attr("id", stop["station"])
+	    .datum(stop);	    
+	})
 
      //create blank array of objects containing agregate information for each t-stop
       stop_list = metaData.map(function(d){
@@ -57,7 +40,7 @@ mbtaVis.prototype.initVis = function() {
           initialized_cat_review[category] = 0
         })
 
-      temp = {  id: d.stop_id,
+      return {  id: d.stop_id,
                 line: d.line,
                 count:0,
                 rating_average:0,
@@ -65,44 +48,37 @@ mbtaVis.prototype.initVis = function() {
                 category_count: initialized_cat_count,
                 category_avg_rating: initialized_cat_rating,
                 category_review_count: initialized_cat_review
-             }  
-
-      return temp   
+             }    
 
      })
 
+		//loop through every restaurant and calculate the total numbers per stop
+		allData.forEach(function(restaurant){
+			stop_list.forEach(function(stop){
 
-    //////////////////
+				if(restaurant.stop_id == stop.id){
+				    stop.count += 1;  //total count
+				    stop.rating_average += restaurant.rating  //total ratings
+				    stop.review_count += restaurant.review_count
 
+				    restaurant.categories.forEach(function(cat){
+						if(category_list.indexOf(cat) >= 0){
+							stop.category_count[cat] += 1;
+							stop.category_avg_rating[cat] += restaurant.rating
+							stop.category_review_count[cat] += restaurant.review_count              
+						}
+				    })
+				}
 
-      //loop through every restaurant and calculate the total numbers per stop
-      allData.forEach(function(restaurant){
-        stop_list.forEach(function(stop){
-
-          if(restaurant.stop_id == stop.id){
-            stop.count += 1;  //total count
-            stop.rating_average += restaurant.rating  //total ratings
-            stop.review_count += restaurant.review_count
-
-            restaurant.categories.forEach(function(cat){
-              if(category_list.indexOf(cat) >= 0){
-                  stop.category_count[cat] += 1;
-                  stop.category_avg_rating[cat] += restaurant.rating
-                  stop.category_review_count[cat] += restaurant.review_count              
-              }
-            })
-
-          }
-
-        })
-      })
+			})
+		})
 
 
-      //divide by number of restaurants in the given stop to create averages when necessary
-      stop_list.forEach(function(stop){
+		//divide by number of restaurants in the given stop to create averages when necessary
+		stop_list.forEach(function(stop){
 
-        //find rating average of total count
-        stop.rating_average = stop.rating_average / stop.count;
+		//find rating average of total count
+		stop.rating_average = stop.rating_average / stop.count;
 
         //find rating average of each category
         for(var key in stop.category_avg_rating){
@@ -115,56 +91,29 @@ mbtaVis.prototype.initVis = function() {
 
       })
 
-      console.log("stop list aggregate", stop_list)
-  }
+      // console.log("stop list aggregate", stop_list)
+      this.stop_list = stop_list;
+      this.update();
+ }
 
 
 
-  //Function to control highlight of specific lines
-  function line_display(){
+mbtaVis.prototype.update = function(){
+	stop_list = this.stop_list;
+	radius_scale = this.radius_scale;
 
-   var e = document.getElementById("mySelectLine");
-   var select_line = e.options[e.selectedIndex].value;
+	//Check for the category that will be displayed
+	var f = document.getElementById("mySelectCat");
+	encode_category = f.options[f.selectedIndex].value;
+	// console.log(encode_category)
 
-   d3.selectAll('circle').style("opacity", .5)
-
-   var line = d3.selectAll('.' + select_line);
-
-   line.style("opacity", 1);
-
-  }
-
-
-  //Update the scaling of the circles based on the user selections
-  function update_vis(){
-
-   //Check how stop will be encoded by (Count, Avg. Rating,Tot. Review)
-   var e = document.getElementById("mySelectOption");
-   var encode_stop_by = e.options[e.selectedIndex].value;
-   console.log(encode_stop_by)
-
-   //Check for the category that will be displayed
-   var f = document.getElementById("mySelectCat");
-   encode_category = f.options[f.selectedIndex].value;
-   console.log(encode_category)
-
-   //Find the domain for the given selection
-   if(encode_stop_by == "count" && encode_category == "all_cat" ){
-    radius_scale.domain([0, d3.max(stop_list, function(d){ return d.count })]) 
-    plot = "count"
-   } else if(encode_stop_by == "average_rating" && encode_category == "all_cat"){
-    radius_scale.domain(d3.extent(stop_list, function(d){ return d.rating_average}))
-    plot = "rating_average"
-   } else if(encode_stop_by == "total_reviews" && encode_category == "all_cat"){
-    radius_scale.domain(d3.extent(stop_list, function(d){ return d.review_count}))
-    plot = "review_count"
-   } else if(encode_stop_by == "count" && encode_category == encode_category){
-    radius_scale.domain(d3.extent(stop_list, function(d){ return d.category_count[encode_category] }))    
-   } else if(encode_stop_by == "average_rating" && encode_category == encode_category){
-    radius_scale.domain(d3.extent(stop_list, function(d){ return d.category_avg_rating[encode_category] }))
-   } else if(encode_stop_by == "total_reviews" && encode_category == encode_category){
-    radius_scale.domain(d3.extent(stop_list, function(d){ return d.category_review_count[encode_category] }))
-   } else {plot = "stop"}
+	//Find the domain for the given selection
+	if(encode_category == "all_cat" ){
+		radius_scale.domain([0, d3.max(stop_list, function(d){ return d.count })]) 
+	} 
+	else {
+		radius_scale.domain(d3.extent(stop_list, function(d){ return d.category_count[encode_category] }))    
+	} 
 
 
     //Update the visualization
@@ -173,26 +122,23 @@ mbtaVis.prototype.initVis = function() {
       var plot_r; //value of plotted radius
 
       stop_list.forEach(function(stop){
-       if(stop.id == d.stop_id){
 
-         if(encode_stop_by == "count" && encode_category == "all_cat" ){
-          plot_r = radius_scale(stop["count"])  
-         } else if(encode_stop_by == "average_rating" && encode_category == "all_cat"){
-          plot_r = radius_scale(stop["rating_average"])
-          console.log(stop["rating_average"], plot_r)
-         } else if(encode_stop_by == "total_reviews" && encode_category == "all_cat"){
-          plot_r = radius_scale(stop["review_count"])
-         } else if(encode_stop_by == "count" && encode_category == encode_category){
-          plot_r = radius_scale(stop.category_count[encode_category])   
-         } else if(encode_stop_by == "average_rating" && encode_category == encode_category){
-          plot_r = radius_scale(stop.category_avg_rating[encode_category])   
-         } else if(encode_stop_by == "total_reviews" && encode_category == encode_category){
-          plot_r = radius_scale(stop.category_review_count[encode_category])   
-         } else {plot_r = 10}
-       }
+      	if(stop.id == d.stop_id){
+
+			if(encode_category == "all_cat" ){
+				plot_r = radius_scale(stop["count"])  
+			} 
+			else if(encode_category == encode_category){
+				plot_r = radius_scale(stop.category_count[encode_category])   
+			} 
+			else {
+				plot_r = 10;
+			}
+        }
 
       })
 
-      return plot_r
+      return plot_r;
+
     })
   }
