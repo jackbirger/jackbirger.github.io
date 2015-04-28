3b87a7358ad86d5c2da94f8421156497f925b9b8
Christian A. Schlubach <christian.schlubach@gmail.com>
2015-04-27 21:41:43 -0400

    adding second set of circles for total reviews.

--- code/js/mbtaVis.js
+++ code/js/mbtaVis.js
@@ -1,54 +1,56 @@
 
 mbtaVis = function(_parentElement, _data, _metaData){
-    this.parentElement = _parentElement;
-    this.data = _data;
-    this.metaData = _metaData;
+    this.parentElement  = _parentElement;
+    this.data           = _data;
+    this.metaData       = _metaData;
 
-	var stop_list = [];  //Aggregate data list
-	this.radius_scale = d3.scale.linear().range([7,75]);
-	//var circles = d3.selectAll('circle')
-	var encode_category='all_cat'; // set this to all categories to start
+	var stop_list       = [];  //Aggregate data list
+	this.radius_scale   = d3.scale.linear().range([7,75]);
+	this.review_scale   = d3.scale.linear().range([7,75]);
+	var encode_category ='all_cat'; // set this to all categories to start
 
 	// draw the initial circles
 	metaData.forEach(function(stop){
 
-	 d3.select("#circles").append('circle')
+	 d3.select("#cat-circles").append('circle')
 	    .attr("cx", stop['x'])
 	    .attr("cy", stop['y'])
 	    .attr("r", 10)
 	    .attr("class", "mbta-circle")
-	    // .attr("class", function(){
-	    //    if(stop.line.length == 1){
-	    //       return stop.line[0];
-	    //    } else{
-	    //       return stop.line[0] + ' ' + stop.line[1]
-	    //    }
-	    // })
 	    .attr("id", stop["station"])
-	    .datum(stop);	    
-	})
+	    .datum(stop);	
+
+	 d3.select("#review-circles").append('circle')
+	    .attr("cx", stop['x'])
+	    .attr("cy", stop['y'])
+	    .attr("r", 5)
+	    .attr("class", "review-circle")
+	    .attr("id", stop["station"])
+	    .datum(stop);	        
+	 })
 
      //create blank array of objects containing agregate information for each t-stop
       stop_list = metaData.map(function(d){
 
-        var initialized_cat_count = {}
+        var initialized_cat_count  = {}
         var initialized_cat_rating = {}
         var initialized_cat_review = {}
 
         category_list.forEach(function(category){
-          initialized_cat_count[category] = 0
+          initialized_cat_count[category]  = 0
           initialized_cat_rating[category] = 0
           initialized_cat_review[category] = 0
         })
 
-      return {  id: d.stop_id,
-                line: d.line,
-                count:0,
-                rating_average:0,
-                review_count:0,
-                category_count: initialized_cat_count,
-                category_avg_rating: initialized_cat_rating,
-                category_review_count: initialized_cat_review
+      return { 
+      	       id                    : d.stop_id,
+ 		       line                  : d.line,
+ 		       count                 : 0,
+ 		       rating_average        : 0,
+ 		       review_count          : 0,
+ 		       category_count        : initialized_cat_count,
+ 		       category_avg_rating   : initialized_cat_rating,
+ 		       category_review_count : initialized_cat_review
              }    
 
      })
@@ -60,7 +62,7 @@ mbtaVis = function(_parentElement, _data, _metaData){
 				if(restaurant.stop_id == stop.id){
 				    stop.count += 1;  //total count
 				    stop.rating_average += restaurant.rating  //total ratings
-				    stop.review_count += restaurant.review_count
+				    stop.review_count += restaurant.review_count;
 
 				    restaurant.categories.forEach(function(cat){
 						if(category_list.indexOf(cat) >= 0){
@@ -102,6 +104,7 @@ mbtaVis = function(_parentElement, _data, _metaData){
 mbtaVis.prototype.update = function(){
 	stop_list = this.stop_list;
 	radius_scale = this.radius_scale;
+	review_scale = this.review_scale;
 
 	//Check for the category that will be displayed
 	var f = document.getElementById("mySelectCat");
@@ -110,29 +113,32 @@ mbtaVis.prototype.update = function(){
 
 	//Find the domain for the given selection
 	if(encode_category == "all_cat" ){
-		radius_scale.domain([0, d3.max(stop_list, function(d){ return d.count })]) 
+		radius_scale.domain([0, d3.max(stop_list, function(d){ return d.count })]);
+		review_scale.domain([0, d3.max(stop_list, function(d){ return d.review_count })]);
 	} 
 	else {
-		radius_scale.domain(d3.extent(stop_list, function(d){ return d.category_count[encode_category] }))    
+		radius_scale.domain(d3.extent(stop_list, function(d){ return d.category_count[encode_category] }));   
+		review_scale.domain(d3.extent(stop_list, function(d){ return d.category_review_count[encode_category] }));  
 	} 
 
-    tot_r = 0; 
-    tot_a = [];
+    tot_r = 0; // total number of restaurants to display in the legend
+    tot_a = [];  // all of the individual data to determine max and min
+
     d3.selectAll('.mbta-circle').transition().duration(1000).attr("r", function(d){
 
       var plot_r; //value of plotted radius
 
-
       stop_list.forEach(function(stop){
 
-      	if(stop.id == d.stop_id){
+      	if (stop.id == d.stop_id){
 
-			if(encode_category == "all_cat" ){
+			if (encode_category == "all_cat" ){
 				plot_r = stop["count"] ? radius_scale(stop["count"]) : 0;
+
 				tot_r  += stop["count"];  
 				tot_a.push(stop["count"]);
 			} 
-			else if(encode_category == encode_category){
+			else if (encode_category == encode_category){
 				var cnt = stop.category_count[encode_category];
 				plot_r  = cnt ? radius_scale(cnt) : 0;
 				tot_r   += cnt; 
@@ -140,9 +146,6 @@ mbtaVis.prototype.update = function(){
 					tot_a.push(cnt);
 				}				  
 			} 
-			else {
-				plot_r = 10;
-			}
         }
 
       })
@@ -157,4 +160,35 @@ mbtaVis.prototype.update = function(){
     d3.select('#total-restaurants').text(tot_r);
     d3.select('#total-max').text(max);
     d3.select('#total-min').text(min);
+
+
+    d3.selectAll('.review-circle').transition().duration(1000).attr("r", function(d){
+
+      var plot_r; //value of plotted radius
+
+      stop_list.forEach(function(stop){
+
+      	if (stop.id == d.stop_id){
+
+			if (encode_category == "all_cat" ){
+				plot_r = stop["count"] ? review_scale(stop["review_count"]) : 0;
+
+				tot_r  += stop["count"];  
+				tot_a.push(stop["count"]);
+			} 
+			else if (encode_category == encode_category){
+				var cnt = stop.category_review_count[encode_category];
+				plot_r  = cnt ? review_scale(cnt) : 0;
+				tot_r   += cnt; 
+				if (cnt>0) {
+					tot_a.push(cnt);
+				}				  
+			} 
+        }
+
+      })
+
+      return plot_r;
+
+    })
   }
