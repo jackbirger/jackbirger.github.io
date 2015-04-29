14dae6d0506d8ede5b283ab8c7bd5c5b028bb26e
Jack Birger <jack.birger@gmail.com>
2015-04-28 21:00:23 -0400

    updates to scatter and zoom

--- code/js/scatterVis.js
+++ code/js/scatterVis.js
@@ -1,16 +1,18 @@
 ScatterVis = function(_parentElement, _data, _metaData, _eventHandler){
-    this.parentElement = _parentElement;
-    this.plotData = _data;
-    this.metaData = _metaData;
-    this.eventHandler = _eventHandler;
+		this.parentElement = _parentElement;
+		this.plotData = _data;
+		this.metaData = _metaData;
+		this.eventHandler = _eventHandler;
 
-    //Margin, width, and height definitions for scatter plot svg
-    this.margin = {top: 20, right: 50, bottom: 200, left: 80},
-    this.width = 780 - this.margin.left - this.margin.right,
-    this.height = 650 - this.margin.top - this.margin.bottom;
+		console.log(this.plotData)
 
-    //Initialize the scatter plot visualization
-    this.initVis();
+		//Margin, width, and height definitions for scatter plot svg
+		this.margin = {top: 20, right: 50, bottom: 100, left: 70},
+		this.width = 780 - this.margin.left - this.margin.right,
+		this.height = 650 - this.margin.top - this.margin.bottom;
+
+		//Initialize the scatter plot visualization
+		this.initVis();
 
 }
 
@@ -23,60 +25,74 @@ ScatterVis.prototype.initVis = function(){
 	var that = this;
 
 	this.svg = d3.select('#scatter').append("svg")
-	    .attr("width", this.width + this.margin.left + this.margin.right)
-	    .attr("height", this.height + this.margin.top + this.margin.bottom)
-	  .append("g")
-	    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+			.attr("width", this.width + this.margin.left + this.margin.right)
+			.attr("height", this.height + this.margin.top + this.margin.bottom)
+		.append("g")
+			.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
 
 	//Define Axis and Scales
 
 	this.x = d3.scale.linear()
-		.range([this.margin.left, this.width - this.margin.left - this.margin.right]);
+		.range([0, this.width]);
 
 	this.y = d3.scale.linear()
-		.range([this.height + this.margin.top, this.margin.top]);
+		.range([this.height, 0]);
 
-  this.xAxis = d3.svg.axis()
-  	.scale(this.x)
-  	.orient("bottom");
+	this.xAxis = d3.svg.axis()
+		.scale(this.x)
+		.orient("bottom");
 
-  this.yAxis = d3.svg.axis()
-    .scale(this.y)
-    .orient("left");
+	this.yAxis = d3.svg.axis()
+		.scale(this.y)
+		.orient("left");
 
-  //g containing the x axis
- 	this.svg.append("g")
-  	.attr("class", "x axis")
-    .attr("transform", "translate(" + -this.margin.left + "," + (this.height + this.margin.top) + ")")
+	//g containing the x axis
+	this.svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + this.height + ")")
 
-  //g containing the y axis
-  this.svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" +0 +"," + 0+")") 
+	//g containing the y axis
+	this.svg.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(0,0)") 
 
-  this.svg.append("g").append("text")
-    .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-    .attr("transform", "translate("+ -50 +","+(this.height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
-    .text("longitude");
+	this.svg.append("g").append("text")
+		.attr("text-anchor", "middle") 
+		.attr("transform", "translate("+ -this.margin.left/1.5 +","+(this.height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+		.text("longitude");
 
 
-  this.svg.append("g").append("text")
-      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-      .attr("transform", "translate("+ ((this.width-this.margin.left-this.margin.right)/2) +","+(this.height+60)+")")  // centre below axis
-      .text("latitude");
+	this.svg.append("g").append("text")
+			.attr("text-anchor", "middle") 
+			.attr("transform", "translate("+ (this.width/2) +","+(this.height + this.margin.bottom/2)+")")  // centre below axis
+			.text("latitude");
 
-  // filter, aggregate, modify data
-  this.wrangleData();
+	// filter, aggregate, modify data
+	this.wrangleData();
 
-  // call the update method
-  this.updateVis();
+	// call the update method
+	this.updateVis();
 
 }
 
 
 
 ScatterVis.prototype.wrangleData = function(){
+
+	//Filter out outlier values (believe they are erroneous values)
+
+	var filter_Data = []
+
+	this.plotData.forEach(function(d){
+
+		if(d.latitude > 42.18 && d.latitude < 42.5 && d.longitude > -71.3 && d.longitude < -70.9){
+			filter_Data.push(d);
+		}
+
+	})
+
+	this.filterData = filter_Data
 
 
 }
@@ -90,98 +106,96 @@ ScatterVis.prototype.updateVis = function(){
 
 	// this.x.domain(d3.extent(this.data, function(d){return d.latitude}))
 
-	this.x.domain([42.2, 42.5])
+	this.x.domain([-71.3, -70.9])
 
 
 	// this.y.domain(d3.extent(this.data, function(d){return d.longitude}))
-	this.y.domain([-70.9, -71.3])
+	this.y.domain([42.18, 42.5])
 
 
-  //update axis
-  this.svg.select(".x.axis")
-    .call(this.xAxis)     
+	//update axis
+	this.svg.select(".x.axis")
+		.call(this.xAxis)     
 
-  this.svg.select(".y.axis")
-    .call(this.yAxis);
-
-
-  //Plot circles for scatter plot
-  this.circle = this.svg
-  						.selectAll("scatter-circle")
-  						.data(this.plotData)
-
- 	this.circle_enter = this.circle
- 							.enter()
- 							.append("circle")
- 							.attr("class", "scatter-circle")
-						   .attr("cx", function(d) {
-						        return that.x(d.latitude);
-						   })
-						   .attr("cy", function(d) {
-						         return that.y(d.longitude);
-						   })
-						   .attr("r", function(d) {
-						         return 2;
-						   })
-						   .style("opacity", 1)
-						   .style("stroke-width", 0)
-
-  var stops = this.svg
-              .selectAll("scatter-stops")
-              .data(this.metaData)
-              .enter()
-              .append("circle")
-              .attr("class", "scatter-stops")
-              .attr("cx", function(d){
-                return that.x(d.ll[0])
-              })
-              .attr("cy", function(d){
-                return that.y(d.ll[1])
-              })
-              .attr("r", function(d){return 3})
-              .style("stroke", "red")
-              .style("fill", "red")
+	this.svg.select(".y.axis")
+		.call(this.yAxis);
 
 
+	//Plot circles for scatter plot
+	this.circle = this.svg
+							.selectAll("scatter-circle")
+							.data(this.filterData)
 
-  var brush = this.svg.append("g")
-      .attr("class", "brush")
-      .call(d3.svg.brush()
-        .x(d3.scale.identity().domain([0, this.width]))
-        .y(d3.scale.identity().domain([0, this.height]))
-        .on("brush", function() {
-          var count = 0
-          var extent = d3.event.target.extent();
-          that.circle_enter.classed("selected", function(d) {
+	this.circle_enter = this.circle
+							.enter()
+							.append("circle")
+							.attr("class", "scatter-circle")
+							 .attr("cx", function(d) {
+										return that.x(d.longitude);
+							 })
+							 .attr("cy", function(d) {
+										 return that.y(d.latitude);
+							 })
+							 .attr("r", function(d) {
+										 return 2;
+							 })
+							 // .style("opacity", 1)
+							 // .style("stroke-width", 0)
 
-            if( extent[0][0] <= that.x(d.latitude) && that.x(d.latitude) < extent[1][0]
-                && extent[0][1] <= that.y(d.longitude) && that.y(d.longitude) < extent[1][1] ){
-              count++;
-              return true
-            }else{return false}
-
-          });
-
-          console.log("count", count)
-
-          var extent = d3.event.target.extent();
-          var extent_invert = [
-                          [that.x.invert(extent[0][0]), that.x.invert(extent[1][0])],
-                          [that.y.invert(extent[0][1]), that.y.invert(extent[1][1])]
-                        ]
-
-
-          $(that.eventHandler).trigger("selectionChanged", extent_invert)
-
-          // if(that.brush.empty()){
-          //  $(that.eventHandler).trigger("selectionChanged", d3.event.target.extent(), , d3.selectAll(".selected"))           
-          // }
-          // else{$(that.eventHandler).trigger("selectionChanged", d3.event.target.extent(), d3.selectAll(".selected"))}  
-
-        }));
+	var stops = this.svg
+							.selectAll("scatter-stops")
+							.data(this.metaData)
+							.enter()
+							.append("circle")
+							.attr("class", "scatter-stops")
+							.attr("cx", function(d){
+								return that.x(d.ll[1])
+							})
+							.attr("cy", function(d){
+								return that.y(d.ll[0])
+							})
+							.attr("r", function(d){return 3})
+							// .style("stroke", "red")
+							// .style("fill", "red")
 
 
-          
+
+	var brush = this.svg.append("g")
+			.attr("class", "brush")
+			.call(d3.svg.brush()
+				.x(d3.scale.identity().domain([0, this.width]))
+				.y(d3.scale.identity().domain([0, this.height]))
+				.on("brush", function() {
+				
+					var extent = d3.event.target.extent();
+					that.circle_enter.classed("selected", function(d) {
+
+						if( extent[0][0] <= that.x(d.longitude) && that.x(d.longitude) < extent[1][0]
+								&& extent[0][1] <= that.y(d.latitude) && that.y(d.latitude) < extent[1][1] ){
+
+							return true
+						}else{return false}
+
+					});
+
+					var extent = d3.event.target.extent();
+					var extent_invert = [
+													[that.x.invert(extent[0][0]), that.x.invert(extent[1][0])],
+													[that.y.invert(extent[0][1]), that.y.invert(extent[1][1])]
+												]
+
+
+					$(that.eventHandler).trigger("selectionChanged", extent_invert)
+
+					// if(that.brush.empty()){
+					//  $(that.eventHandler).trigger("selectionChanged", d3.event.target.extent(), , d3.selectAll(".selected"))           
+					// }
+					// else{$(that.eventHandler).trigger("selectionChanged", d3.event.target.extent(), d3.selectAll(".selected"))}  
+
+				}));
+
+
+					
 }
 
 
--- code/js/zoomVis.js
+++ code/js/zoomVis.js
@@ -1,15 +1,15 @@
 ZoomVis = function(_parentElement, _data, _metaData){
-    this.parentElement = _parentElement;
-    this.data = _data;
-    this.metaData = _metaData;
+	this.parentElement = _parentElement;
+	this.data = _data;
+	this.metaData = _metaData;
 
-    //Margin, width, and height definitions for scatter plot svg
-    this.margin = {top: 20, right: 20, bottom: 50, left: 50},
-    this.width = 350 - this.margin.left - this.margin.right,
-    this.height = 400 - this.margin.top - this.margin.bottom;
+	//Margin, width, and height definitions for scatter plot svg
+	this.margin = {top: 20, right: 50, bottom: 100, left: 70},
+	this.width = 780 - this.margin.left - this.margin.right,
+	this.height = 650 - this.margin.top - this.margin.bottom;
 
-    //Initialize the scatter plot visualization
-    this.initVis();
+	//Initialize the scatter plot visualization
+	this.initVis();
 
 }
 
@@ -20,42 +20,50 @@ ZoomVis.prototype.initVis = function(){
 
 	var that = this;
 
+	//define svg
 	this.svg = d3.select('#scatter-zoom').append("svg")
-	    .attr("width", this.width + this.margin.left + this.margin.right)
-	    .attr("height", this.height + this.margin.top + this.margin.bottom)
+		.attr("width", this.width + this.margin.left + this.margin.right)
+		.attr("height", this.height + this.margin.top + this.margin.bottom)
 	  .append("g")
-	    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+		.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
 
-	//Define Axis and Scales
-
+	//Define Scales
 	this.x = d3.scale.linear()
 		.range([0, this.width]);
 
 	this.y = d3.scale.linear()
 		.range([this.height, 0]);
 
-  	this.xAxis = d3.svg.axis()
-	  	.scale(this.x)
-	  	.orient("bottom");
+	//Define Axes
+	this.xAxis = d3.svg.axis()
+		.scale(this.x)
+		.orient("bottom");
 
 	  this.yAxis = d3.svg.axis()
-	    .scale(this.y)
-	    .orient("left");
+		.scale(this.y)
+		.orient("left");
 
-  	//g containing the x axis
- 	this.svg.append("g")
-  		.attr("class", "x axis")
-    	.attr("transform", "translate(0," + this.height + ")")
+	//g containing the x axis
+	this.svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + this.height + ")")
 
-	  //g containing the y axis
-	  this.svg.append("g")
-	      .attr("class", "y axis")
-	      .attr("transform", "translate(0,0)")  
+	//g containing the y axis
+	this.svg.append("g")
+	  .attr("class", "y axis")
+	  .attr("transform", "translate(0,0)")  
+
+	this.svg.append("g").append("text")
+		.attr("text-anchor", "middle") 
+		.attr("transform", "translate("+ -this.margin.left/1.5 +","+(this.height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+		.text("longitude");
 
 
-  // filter, aggregate, modify data
-  this.wrangleData();
+	this.svg.append("g").append("text")
+			.attr("text-anchor", "middle") 
+			.attr("transform", "translate("+ (this.width/2) +","+(this.height + this.margin.bottom/2)+")")  // centre below axis
+			.text("latitude");
 
   // call the update method
 
@@ -63,9 +71,40 @@ ZoomVis.prototype.initVis = function(){
 }
 
 
-ZoomVis.prototype.wrangleData = function(){
+//Filter yelp dataset and the list of stops from the metadata
+//so that only consists of the subset of data that is within
+//The bounds of the brush select
+ZoomVis.prototype.wrangleData = function(x_extents, y_extents){
+
+	var that = this;
 
 
+	//Filter restaurant data and store in this.plotData
+	this.plotData = [];
+
+	this.data.forEach(function(d){
+
+		if( x_extents[0] < d.longitude && d.longitude < x_extents[1]
+				&& y_extents[0] > d.latitude && d.latitude > y_extents[1]){
+			
+			that.plotData.push(d)
+		}
+	})
+
+
+
+	//Filter train stops and store in this.plotStops
+	this.plotStops = [];
+	this.metaData.forEach(function(d){
+
+		if( x_extents[0] < d.ll[1] && d.ll[1] < x_extents[1]
+				&& y_extents[0] > d.ll[0] && d.ll[0] > y_extents[1]){
+			
+			that.plotStops.push(d)
+		}
+	})
+
+	//console.log("plotted stops", this.plotStops)
 
 }
 
@@ -73,33 +112,11 @@ ZoomVis.prototype.wrangleData = function(){
 
 ZoomVis.prototype.onSelectionChange = function (x_extents, y_extents){
 
+
 	var that = this;
 
-	this.plotData = [];
-
-	this.data.forEach(function(d){
-
-		if( x_extents[0] <= d.latitude && d.latitude < x_extents[1]
-                && y_extents[0] <= d.longitude && d.longitude < y_extents[1]){
-			
-			that.plotData.push(d)
-		}
-	})
-
-	console.log("plotData", this.plotData)
-
-	this.plotStops = [];
-
-	this.metaData.forEach(function(d){
-
-		if( x_extents[0] <= d.ll[0] && d.ll[0] < x_extents[1]
-                && y_extents[0] <= d.ll[1] && d.ll[1] < y_extents[1]){
-			
-			that.plotStops.push(d)
-		}
-	})
-
-	console.log("plotted stops", this.plotStops)
+	// filter, aggregate, modify data
+	this.wrangleData(x_extents, y_extents);
 
 
 	this.x.domain([x_extents[0], x_extents[1]])
@@ -108,62 +125,61 @@ ZoomVis.prototype.onSelectionChange = function (x_extents, y_extents){
 	//update axis
 	this.svg.select(".x.axis")
 		.call(this.xAxis)
-      .selectAll("text")  
-	    .style("text-anchor", "end")
-	    .attr("dx", "-.8em")
-	    .attr("dy", ".15em")
-	    .attr("transform", function(d) {
-	        return "rotate(-65)" 
-	     });      
+	  .selectAll("text")  
+		.style("text-anchor", "end")
+		.attr("dx", "-.8em")
+		.attr("dy", ".15em")
+		.attr("transform", function(d) {
+			return "rotate(-65)" 
+		 });      
 
 
+	this.svg.select(".y.axis")
+		.call(this.yAxis);
 
-  	this.svg.select(".y.axis")
-    	.call(this.yAxis);
 
-
-  	//Plot circles for scatter plot
-  	this.circle = this.svg
+	//Plot circles for scatter plot
+	this.circle = this.svg
 		.selectAll(".brush-circles")
 		.data(this.plotData)
 
- 	this.circle_enter = this.circle
-			.enter()
-			.append("circle")	
-			.attr("class", "brush-circles")
-			.attr("cx", function(d) {
-	        	return that.x(d.latitude);
-	 		})
-	  		.attr("cy", function(d) {
-	        	return that.y(d.longitude);
-	 		})
-	  		.attr("r", function(d) {
-	        	return 2;
-	   		})
-	   		.style("opacity", 1)
-	   		.style("stroke-width", 1)
+
+	this.circle_enter = this.circle
+		.enter()
+		.append("circle")	
+		.attr("class", "brush-circles")
+		.attr("cx", function(d) {
+			return that.x(d.longitude);
+		})
+		.attr("cy", function(d) {
+			return that.y(d.latitude);
+		})
+		.attr("r", function(d) {
+			return 2;
+		})
+		// .style("opacity", 1)
+		// .style("stroke-width", 1)
 
 	this.circle_exit = this.circle.exit().remove();
 
+	this.stops = this.svg
+		.selectAll(".brush-stops")
+		.data(this.plotStops)
 
-  this.stops = this.svg
-              .selectAll(".brush-stops")
-              .data(this.plotStops);
+	this.stops_enter = this.stops.enter()
+		.append("circle")
+		.attr("class", "brush-stops")
+		.attr("cx", function(d){
+			return that.x(d.ll[1])
+		})
+		.attr("cy", function(d){
+			return that.y(d.ll[0])
+		})
+		.attr("r", function(d){return 3})
+		// .style("stroke", "red")
+		// .style("fill", "red");
 
-  this.stops_enter = this.stops.enter()
-              .append("circle")
-              .attr("class", "brush-stops")
-              .attr("cx", function(d){
-                return that.x(d.ll[0])
-              })
-              .attr("cy", function(d){
-                return that.y(d.ll[1])
-              })
-              .attr("r", function(d){return 3})
-              .style("stroke", "red")
-              .style("fill", "red");
-
-   this.stops_exit = this.stops.exit().remove();
+	this.stops_exit = this.stops.exit().remove();
 
 }
 
