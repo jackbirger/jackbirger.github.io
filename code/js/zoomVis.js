8eeaf6f222f467bda5e8f0e6353bb2f39c473fac
Jack Birger <jack.birger@gmail.com>
2015-05-04 18:10:57 -0400

    zoomVis refactoring for checkboxes

--- code/js/zoomVis.js
+++ code/js/zoomVis.js
@@ -8,10 +8,12 @@ ZoomVis = function(_parentElement, _data, _metaData, _eventHandler2){
 	this.margin = {top: 20, right: 10, bottom: 10, left: 70, padding:10},
 	this.width = 300 - this.margin.left - this.margin.right,
 	this.height = 250 - this.margin.top - this.margin.bottom;
+	this.padding = 2;
 
 	//Initialize the scatter plot visualization
 	this.initVis();
 	this.showOption = 'none';
+
 }
 
 /**
@@ -20,7 +22,6 @@ ZoomVis = function(_parentElement, _data, _metaData, _eventHandler2){
 ZoomVis.prototype.initVis = function(){
 
 	var that = this;
-
 
 	//define svg
 	this.svg = d3.select('#scatter-zoom').append("svg")
@@ -32,10 +33,10 @@ ZoomVis.prototype.initVis = function(){
 
 	//Define Scales
 	this.x = d3.scale.linear()
-		.range([2, this.width-2]);
+		.range([this.padding, this.width-this.padding]);
 
 	this.y = d3.scale.linear()
-		.range([this.height-2, 2]);
+		.range([this.height-this.padding, this.padding]);
 
 	//Define Axes
 	this.xAxis = d3.svg.axis()
@@ -46,15 +47,22 @@ ZoomVis.prototype.initVis = function(){
 		.scale(this.y)
 		.orient("left");
 
-	//create rectangle for the zoom view
-
+	//create border rectangle for the zoom view
 	this.svg.append("rect").attr("class", "border")
 		.attr("x", 0)
 		.attr("y", 0)
 		.attr("width", this.width)
 		.attr("height", this.height)
 
+	//groups for zoom layout elements
+	this.svg.append("g")
+			.attr("id", "g-brush-circles")
 
+	this.svg.append("g")
+			.attr("id", "g-brush-coffee")
+
+	this.svg.append("g")
+			.attr("id", "g-brush-stops")
 }
 
 
@@ -65,11 +73,14 @@ ZoomVis.prototype.wrangleData = function(x_extents, y_extents){
 
 	var that = this;
 
-	//Data within the zoom extents
+	//Data within the brush extents
 	this.plotData = [];
 
-	//Common Restaurants within the zoom extents
-	this.commonRestaurants = [];
+	//Coffee shops within the brush extents
+	this.coffeeRestaurants = [];
+
+	//Train stops within the brush extents
+	this.plotStops = [];
 
 
 	this.data.forEach(function(d){
@@ -77,24 +88,18 @@ ZoomVis.prototype.wrangleData = function(x_extents, y_extents){
 		if( x_extents[0] < d.longitude && d.longitude < x_extents[1]
 				&& y_extents[0] > d.latitude && d.latitude > y_extents[1]){
 
+			//Create list of restaurants within extents
 			that.plotData.push(d)
 
-			if(d.name == "Chipotle Mexican Grill"){
-				that.commonRestaurants.push(d);
-			} else if(d.name == "Dunkin' Donuts"){
-				that.commonRestaurants.push(d);
-			} else if(d.name == "Starbucks"){
-				that.commonRestaurants.push(d);
-			} else if(d.name == "Au Bon Pain"){
-				that.commonRestaurants.push(d);
-			}
+			//Create list of coffee shops within the extents
+			if(d.name == "Dunkin' Donuts"){ that.coffeeRestaurants.push(d) }
+			else if(d.name == "Starbucks"){ that.coffeeRestaurants.push(d) }	
 
 		}
 	})
 
 
-	//Filter train stops and store in this.plotStops
-	this.plotStops = [];
+	//Populate this.plotStops with train stops in brush extents
 	this.metaData.forEach(function(d){
 
 		if( x_extents[0] < d.ll[1] && d.ll[1] < x_extents[1]
@@ -104,9 +109,9 @@ ZoomVis.prototype.wrangleData = function(x_extents, y_extents){
 		}
 	})
 
-	//console.log("plotted stops", this.plotData)
-	//console.log("commonRestaurants", this.commonRestaurants)
-
+	//console.log("plotted stops", this.plotStops)
+	// console.log("coffeeRestaurants", this.coffeeRestaurants)
+	// console.log("plotData", this.plotData)
 }
 
 ZoomVis.prototype.setOption = function(select_options) {
@@ -117,72 +122,38 @@ ZoomVis.prototype.onSelectionChange = function (x_extents, y_extents){
 
 	var that = this;
 
-	// filter, aggregate, modify data
+	// wrangle data that will be updated in brush zoom
 	this.wrangleData(x_extents, y_extents);
 
 	//might want to move following stuff into new function to neaten up
 	if(this.plotData.length > 0){
 
-	this.x.domain([x_extents[0], x_extents[1]])
-	this.y.domain([y_extents[1], y_extents[0]])
+		//Set Scale Domain
+		this.x.domain([x_extents[0], x_extents[1]])
+		this.y.domain([y_extents[1], y_extents[0]])
 
 
-
-	//Plot circles for scatter plot
-	this.circle = this.svg
-		.selectAll(".brush-circles")
-		.data(this.plotData)
-
-	this.circle
-		.attr("cx", function(d) {
-			return that.x(d.longitude);
-		})
-		.attr("cy", function(d) {
-			return that.y(d.latitude);
-		})
-		.attr("r", function(d) {
-			return 2;
-		})
+		// this.drawZoomRestaurants();
+		// this.drawZoomStops();
+		// this.drawZoomCoffee();
 
 
-	this.circle_enter = this.circle
-		.enter()
-		.append("circle")	
-		.attr("class", "brush-circles")
-		.attr("cx", function(d) {
-			return that.x(d.longitude);
-		})
-		.attr("cy", function(d) {
-			return that.y(d.latitude);
-		})
-		.attr("r", function(d) {
-			return 2;
-		})
-
-
-	this.circle_exit = this.circle.exit().remove();
 	
-
-	if (this.showOption == 'stops') {
-			console.log("hereherehere");
-
-			that.drawZoomStops();	  	
- 	}
-    else if (this.showOption == 'none') {
-    	// console.log('not');
-    }
-    else if (this.showOption == 'common') {
-    	console.log('common');
-    	that.drawZoomCommon();
-    }
-
+	d3.selectAll("input").each(function(d) { 
+        if (d3.select(this).attr("type") == "checkbox" && d3.select(this).attr("name") == "stops" && d3.select(this).node().checked) {
+        	that.drawZoomStops();
+        }
+        else if (d3.select(this).attr("type") == "checkbox" && d3.select(this).attr("name") == "coffee" && d3.select(this).node().checked) {
+        	that.drawZoomCoffee();
+        }
+        else if (d3.select(this).attr("type") == "checkbox" && d3.select(this).attr("name") == "universities" && d3.select(this).node().checked) {
+        	console.log("Implement universities")
+        }
+        else if (d3.select(this).attr("type") == "checkbox" && d3.select(this).attr("name") == "all" && d3.select(this).node().checked) {
+        	that.drawZoomRestaurants();
+        }
+    });
 	    
-
-
-
-
-
-
 
 
 	$(that.eventHandler_2).trigger("brushChanged", [that.plotData, that.plotStops]);
@@ -193,18 +164,16 @@ ZoomVis.prototype.onSelectionChange = function (x_extents, y_extents){
 }
 
 
-
+//Draws and updates the train stops in the zoom layout
 ZoomVis.prototype.drawZoomStops = function(){
+
 
 	var that = this;
 
-	var stops = this.svg
-		.selectAll(".brush-stops")
-		.data(this.plotStops)
-		// .attr("class", function(d){
-		// 	return "brush-stops " + d.line[0];
-		// })
+	g = d3.select('#g-brush-stops');
 
+	var stops = g.selectAll(".brush-stops")
+		.data(this.plotStops)
 
 	stops
 		.attr("cx", function(d){
@@ -238,27 +207,20 @@ ZoomVis.prototype.drawZoomStops = function(){
 }
 
 
-ZoomVis.prototype.drawZoomCommon = function(){
-
-	console.log("here")
+//Draws and updates the coffee shops in the zoom layout
+ZoomVis.prototype.drawZoomCoffee = function(){
 
 
 	var that = this;
 
-	var stops = this.svg
-		.selectAll(".brush-common")
-		.data(this.commonRestaurants)
+	g = d3.select('#g-brush-coffee');
+
+	var stops = g.selectAll(".brush-coffee")
+		.data(this.coffeeRestaurants)
 		.attr("class", function(d){
-			if(d.name == "Chipotle Mexican Grill"){
-				return "brush-common " + "Chipotle";
-			} else if(d.name == "Dunkin' Donuts"){
-				return "brush-common " + "Dunkin";
-			} else if(d.name == "Starbucks"){
-				return "brush-common " + "Starbucks";
-			} else if(d.name == "Au Bon Pain"){
-				return "brush-common " + "AuBonPain";
-			}
-		});
+			if(d.name == "Dunkin' Donuts"){ return "brush-coffee " + "Starbucks" }
+			else if(d.name == "Starbucks"){ return "brush-coffee " + "Dunkin" }
+		})
 
 	stops
 		.attr("cx", function(d){
@@ -273,15 +235,8 @@ ZoomVis.prototype.drawZoomCommon = function(){
 		.enter()
 		.append("circle")
 		.attr("class", function(d){
-			if(d.name == "Chipotle Mexican Grill"){
-				return "brush-common " + "Chipotle";
-			} else if(d.name == "Dunkin' Donuts"){
-				return "brush-common " + "Dunkin";
-			} else if(d.name == "Starbucks"){
-				return "brush-common " + "Starbucks";
-			} else if(d.name == "Au Bon Pain"){
-				return "brush-common " + "AuBonPain";
-			}
+			if(d.name == "Dunkin' Donuts"){ return "brush-coffee " + "Starbucks" }
+			else if(d.name == "Starbucks"){ return "brush-coffee " + "Dunkin" }
 		})
 		.attr("cx", function(d){
 			return that.x(d.longitude)
@@ -293,6 +248,49 @@ ZoomVis.prototype.drawZoomCommon = function(){
 
 
 	var stops_exit = stops.exit().remove();
+}
+
+
+//Draws and updates the Restaurants in the zoom layout
+ZoomVis.prototype.drawZoomRestaurants = function(){
+
+	var that = this;
+
+	g = d3.select('#g-brush-circles');
+
+	//Plot circles for scatter plot
+	this.circle = g.selectAll(".brush-circles")
+		.data(this.plotData)
+
+	this.circle
+		.attr("cx", function(d) {
+			return that.x(d.longitude);
+		})
+		.attr("cy", function(d) {
+			return that.y(d.latitude);
+		})
+		.attr("r", function(d) {
+			return 2;
+		})
+
+
+	this.circle_enter = this.circle
+		.enter()
+		.append("circle")	
+		.attr("class", "brush-circles")
+		.attr("cx", function(d) {
+			return that.x(d.longitude);
+		})
+		.attr("cy", function(d) {
+			return that.y(d.latitude);
+		})
+		.attr("r", function(d) {
+			return 2;
+		})
+
+
+	this.circle_exit = this.circle.exit().remove();
+
 }
 
 
